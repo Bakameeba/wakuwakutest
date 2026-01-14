66018a411f624f637cc56668205d9574fd62b3b0
4757g6pmjz-cmd <4757g6pmjz@privaterelay.appleid.com>
2026-01-14 08:23:39 +0000

    a

--- new-test1/main.py
+++ new-test1/main.py
@@ -1,22 +1,32 @@
 import pyxel
-
-
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
-        pyxel.load("my_resource.pyxres")
-        self.x = 0
-        pyxel.run(self.update, self.draw)
-
+        pyxel.init(256, 256)
+        self.x, self.y = 48, 48
+        self.color = pyxel.COLOR_GREEN
+        pyxel.run(self.update, self.draw)    
     def update(self):
-        self.x += 1
-        if self.x >= pyxel.width:
-            self.x = -8
-
+        #十字キー
+        if pyxel.btnp(pyxel.KEY_DOWN):
+            self.y -= 2
+        if pyxel.btnp(pyxel.KEY_UP):
+            self.y += 2
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.x -= 2
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.x += 2
+        #日本でいうところのAとB、XとYがそれぞれ逆なので注意
+        if pyxel.btnp(pyxel.KEY_D):
+            self.color = pyxel.COLOR_RED
+        if pyxel.btnp(pyxel.KEY_C):
+            self.color = pyxel.COLOR_GREEN
+        if pyxel.btnp(pyxel.KEY_B):
+            self.color = pyxel.COLOR_YELLOW
+        if pyxel.btnp(pyxel.KEY_A):
+            self.color = pyxel.COLOR_CYAN
+        
     def draw(self):
-        pyxel.cls(0)
-        pyxel.blt(self.x, 60, 0, 0, 0, 8, 8, 2)
-        pyxel.text(48, 53, "Pyxel Code Maker", pyxel.rndi(1, 15))
-
-
-App()
+        pyxel.cls(7)
+        pyxel.rect(self.x, self.y, 32, 32, self.color)
+if __name__ == '__main__':
+    app = App()
